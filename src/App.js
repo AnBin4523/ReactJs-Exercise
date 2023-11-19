c9158c1b90e6a57a16cb4e7cb332dfa5e23ff539
An Bin <quocthienhong@gmail.com>
2023-11-19 22:19:59 +0700

    initial exercise

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ShoeStore from './ReactJs-Shoe-Shop/ShoeStore';
+
+function App() {
+  return (
+    <ShoeStore />
+  );
+}
+
+export default App;
---
+++ src/ReactJs-Shoe-Shop/ShoeStore.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import dataShoe from "./dataShoe.json";
+
+export default function ShoeStore() {
+  return <div>ShoeStore</div>;
+}
